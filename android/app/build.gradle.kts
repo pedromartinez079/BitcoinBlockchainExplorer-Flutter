2e86a0855c73c50f35cb95cb6ba2bd46d79de8fd
unknown <pedro.martinezlr@gmail.com>
2026-01-27 17:15:04 -0500

    VersionCode2 VersionName 1.0.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         // Check https://apilevels.com/
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     signingConfigs {
